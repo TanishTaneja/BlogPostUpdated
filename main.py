32b40f85c7cd410804da1aff5f869c3b7997bfca
TanishTaneja <thetanutaneja@gmail.com>
2023-08-23 01:46:16 +0530

    req updated

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///blog.db') 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI") 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
