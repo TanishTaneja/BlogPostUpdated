ef85f0bf293d3d3f5f301f75703af2b3db7d3dcd
TanishTaneja <thetanutaneja@gmail.com>
2024-07-10 23:29:25 +0530

    requirements reinitiated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort, request
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap4
 from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -57,11 +57,11 @@ gravatar = Gravatar(
 
 app.config['SECRET_KEY'] = os.environ.get("CSRF_TOKEN")
 ckeditor = CKEditor(app)
-Bootstrap(app)
+Bootstrap4(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///blog.db') 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' 
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///blog.db') 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
